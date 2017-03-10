f919dbf8ae218a88a68313ad3c36e8332b0b24d2
Luiz de Milon <luiz.de.milon@gmail.com>
2017-03-10 14:57:01 -0300

    add nothing?, just? Unwrap

--- maybe.scm
+++ maybe.scm
@@ -1,7 +1,23 @@
 (define Just
   (lambda (a)
-    (list 'just a)))
+    (list 'Just a)))
 
 (define Nothing
   (lambda ()
     'Nothing))
+
+(define nothing?
+  (lambda (a)
+    (equal? 'Nothing a)))
+
+(define just?
+  (lambda (a)
+    (and (list? a)
+         (equal? 'Just (car a)))))
+
+(define Unwrap
+  (lambda (a)
+    (cond
+      ((nothing? a) a)
+      ((just? a) (cadr a))
+      (else (Nothing)))))
